78db99ed3f956e65428e208ba4a8b1278b976bd5
Ibrahim Khan <aitvetibrahim@gmail.com>
2023-04-05 00:38:33 +0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  TextEditingController firstName = TextEditingController();
+  TextEditingController lastName = TextEditingController();
+  TextEditingController phone = TextEditingController();
+  TextEditingController address = TextEditingController();
+  bool editMode = false;
+
+  Future getData()async{
+    var url = 'http://192.168.0.104/php_mysql_crud/read.php';
+    var response = await http.get(Uri.parse(url));
+    return json.decode(response.body);
+  }
+
+  // Future<void> insertrecored() async {
+  //   if (name.text != "" || email.text != "" || password.text != "") {
+  //     try {
+  //       String uri = "http://localhost/practice_api/insert_record.php";
+  //       var res = await http.post(Uri.parse(uri),body: {
+  //         "name":name.text,
+  //         "email":email.text,
+  //         "password":password.text
+  //       });
+  //       var response = jsonDecode(res.body);
+  //       if(response["success"]=="true"){
+  //         print("Record Inserted");
+  //       }else{
+  //         print("some issue");
+  //       }
+  //     } catch (e) {
+  //       print(e);
+  //     }
+  //   } else {
+  //     print("please Fill All fields");
+  //   }
+  // }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Insert Record'),
+        ),
+        body: FutureBuilder(
+          future: getData(),
+          builder: (context, snapshot){
+            if(snapshot.hasError) print(snapshot.error);
+            return snapshot.hasData ? ListView.builder(
+              itemCount: snapshot.data.length,
+              itemBuilder: (context, index){
+                List list = snapshot.data;
+                return ListTile(
+                  leading: GestureDetector(
+                    child: Icon(Icons.edit),
+                    onTap: (){
+                      debugPrint('Edit Clicked');
+                    },
+                  ),
+                  title: Text('${list[index]['firstname']} ${list[index]['lastname']}'),
+                  subtitle: Text('${list[index]['phone']}'),
+                  trailing:  GestureDetector(
+                    child: Icon(Icons.delete),
+                    onTap: (){
+                      debugPrint('Delete Clicked');
+                    },
+                  ),
+                );
+              },
+            ) : CircularProgressIndicator();
+          },
+        ),
+
+
+        // Column(
+        //   children: [
+        //     Container(
+        //       margin: EdgeInsets.all(20),
+        //       child: TextFormField(
+        //         controller: firstName,
+        //         decoration: InputDecoration(
+        //             border: OutlineInputBorder(),
+        //             label: Text('Enter the first Name')),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.all(20),
+        //       child: TextFormField(
+        //         controller: lastName,
+        //         decoration: InputDecoration(
+        //             border: OutlineInputBorder(),
+        //             label: Text('Enter the Last Name')),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.all(20),
+        //       child: TextFormField(
+        //         controller: phone,
+        //         decoration: InputDecoration(
+        //             border: OutlineInputBorder(), label: Text('Enter Phone')),
+        //       ),
+        //     ),
+        //     Container(
+        //       margin: EdgeInsets.all(20),
+        //       child: TextFormField(
+        //         controller: address,
+        //         decoration: InputDecoration(
+        //             border: OutlineInputBorder(),
+        //             label: Text('Enter address')),
+        //       ),
+        //     ),
+        //     ElevatedButton(
+        //         onPressed: () {
+        //          // insertrecored();
+        //         },
+        //         child: Text('Insert'))
+        //   ],
+        // ),
+      ),
+    );
+  }
+}
